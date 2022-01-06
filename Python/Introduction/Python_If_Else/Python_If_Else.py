9ba00ab2cfb02431fe75f15b3acdceb4a097507f
Ekaterina Litvinova <yalitvi@outlook.com>
2022-01-06 14:41:28 +0100

    Add an exercise

---
+++ Python/Introduction/Python_If_Else/Python_If_Else.py
@@ -0,0 +1,8 @@
+if n%2!=0 and 1<=n<=100:
+    print('Weird')
+elif n%2==0 and 1<=n<=100 and 2<=n<=5:
+    print('Not Weird')
+elif n%2==0 and 1<=n<=100 and 6<=n<=20:
+    print('Weird')
+elif n%2==0 and 1<=n<=100 and n>20:
+    print('Not Weird')
